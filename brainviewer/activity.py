e7e67615d35ae9846a60b3f41b72c316f07dcb61
EmeEmu <matteo.d.kott@gmail.com>
2025-01-30 14:40:20 +0100

    additions for CaIman routine

--- brainviewer/activity.py
+++ brainviewer/activity.py
@@ -36,8 +36,16 @@ class ActivityViewer:
         self.update()
 
     def rescale_y(self):
-        mins = [l.get_ydata().min() for l in self.ax.lines]
-        maxs = [l.get_ydata().max() for l in self.ax.lines]
+        mins = [
+            l.get_ydata().min()
+            for l in self.ax.lines
+            if isinstance(l.get_ydata(), np.ndarray)
+        ]
+        maxs = [
+            l.get_ydata().max()
+            for l in self.ax.lines
+            if isinstance(l.get_ydata(), np.ndarray)
+        ]
         mmin = np.min(mins)
         mmax = np.max(maxs)
         self.ax.set_ylim(mmin, mmax)
@@ -89,7 +97,69 @@ class PointLayerSelector:
         show_info("Right Click on a neuron to display its activity.")
 
 
+class ContourLayerSelector:
+    def __init__(self, layer, widget, activities, labels=None):
+        self.layer = layer
+        assert isinstance(layer, napari.layers.shapes.shapes.Shapes)
+        assert hasattr(layer, "ids")
+        self.widget = widget
+
+        # compute center of mass for each contour
+        COMS = []
+        for i in range(len(np.unique(layer.ids))):
+            idxs = np.where(layer.ids == i)[0]
+            contours = np.concatenate([layer.data[j] for j in idxs])
+            com = np.mean(contours, axis=0)
+            COMS.append(com)
+        self.COMs = np.array(COMS)
+
+        if isinstance(activities, list):
+            self.activities = activities
+        elif isinstance(activities, np.ndarray):
+            self.activities = [activities]
+        else:
+            raise TypeError()
+
+        if labels is None:
+            self.labels = None
+        else:
+            assert len(labels) == len(self.activities)
+            self.labels = labels
+
+        # checking if activities have the correct nb of elements
+        for a in self.activities:
+            assert len(np.unique(self.layer.ids)) == a.shape[1]
+
+        # prepare lines for the plot
+        self.lines = []
+        for a in self.activities:
+            (line,) = self.widget.ax.plot(a[:, 0])
+            self.lines.append(line)
+        change_shape_colors(self.layer, 0)
+
+        @self.layer.mouse_drag_callbacks.append
+        def click_finder(l, e):
+            if e.button != 2:
+                return
+            pos = l.world_to_data(e.position)
+            dists = np.sum((self.COMs - pos) ** 2, axis=1)
+            i = np.argmin(dists)
+            change_shape_colors(l, i)
+            for j in range(len(self.activities)):
+                self.lines[j].set_ydata(self.activities[j][:, i])
+            self.widget.rescale_y()
+
+        show_info("Right Click on a neuron to display its activity.")
+
+
 def change_point_colors(layer, i):
     colors = np.ones((layer.data.shape[0], 4))
     colors[i, :] = [1, 0, 0, 1]
+    layer.face_color = colors
+
+
+def change_shape_colors(layer, i):
+    idxs = np.where(layer.ids == i)[0]
+    colors = np.ones((len(layer.ids), 4))
+    colors[idxs, :] = [1, 0, 0, 1]
     layer.face_color = colors
--- brainviewer/viewer.py
+++ brainviewer/viewer.py
@@ -2,6 +2,8 @@ from pathlib import Path
 
 import napari
 import numpy as np
+import zarr
+from napari.utils.notifications import show_info, show_warning
 from qtpy.QtWidgets import QFileDialog
 
 from .hdf5_handling import HDF5_Browser
@@ -26,13 +28,20 @@ class NapariBrainViewer:
         data_menu.addAction("HDF5 (.h5)", self.load_hdf5)
 
         # setting up the viewer
-        self._set_dimensions()
+        # self._set_dimensions()
 
-
-    def _set_dimensions(self):
+    def _set_dimensions(self, ndims=4):
         d = self._viewer.dims
-        d.ndim = 4
-        d.axis_labels = ["t", "z", "x", "y"]
+        if ndims == 3:
+            d.ndim = 3
+            d.axis_labels = ["t", "x", "y"]
+            d.order = [0, 1, 2]
+        elif ndims == 4:
+            d.ndim = 4
+            d.axis_labels = ["z", "t", "x", "y"]
+            d.order = [1, 0, 2, 3]
+        else:
+            raise NotImplementedError()
 
     def _ui_select_files(self, prompt):
         files = self._viewer.window._qt_viewer._open_file_dialog_uni(prompt)
@@ -71,7 +80,7 @@ class NapariBrainViewer:
     ):
         assert isinstance(coords, np.ndarray)
         assert coords.ndim == 2
-        assert coords.shape[1] == 3
+        # assert coords.shape[1] == 3
 
         if size is None:
             # if self.pixel_size is not None:
@@ -84,7 +93,7 @@ class NapariBrainViewer:
                 coords,
                 size=size,
                 out_of_slice_display=True,
-                units=(self.space_unit,) * 3,
+                # units=(self.space_unit,) * 3,
                 **kwargs,
             )
         else:
@@ -103,6 +112,73 @@ class NapariBrainViewer:
                 **kwargs,
             )
 
+        return layer
+
+    def contours(self, contours, **kwargs):
+        layer = self._viewer.add_shapes(name="contours")
+        colors = np.random.rand(len(contours), 3)
+        ids = []
+        for i in range(len(contours)):
+            contour = contours[i]
+            for c in contour:
+                layer.add_polygons(
+                    c,
+                    face_color=colors[i],
+                    edge_width=0,
+                    **kwargs,
+                )
+                ids.append(i)
+
+        layer.ids = np.array(ids)
+        return layer
+
+    def image(self, img_arr, cmap="inferno", clims=None, **kwargs):
+        assert isinstance(img_arr, np.ndarray)
+        assert img_arr.ndim == 2
+
+        if clims is None:
+            clims = np.quantile(img_arr, [0.05, 0.95])
+
+        layer = self._viewer.add_image(
+            img_arr,
+            contrast_limits=clims,
+            colormap=cmap,
+            **kwargs,
+        )
+        return layer
+
+    def stack(self, stack_arr, cmap="inferno", clims=None, **kwargs):
+        assert isinstance(stack_arr, np.ndarray)
+        assert stack_arr.ndim == 3
+        if clims is None:
+            clims = np.quantile(
+                stack_arr[stack_arr.shape[0] // 2, :, :],
+                [0.5, 0.9999],
+            )
+        layer = self._viewer.add_image(
+            stack_arr,
+            contrast_limits=clims,
+            multiscale=False,
+            colormap=cmap,
+            **kwargs,
+        )
+        return layer
+
+    def hyperstack(self, hstack_arr, cmap="inferno", **kwargs):
+        assert isinstance(hstack_arr, (np.ndarray, zarr.core.Array))
+        assert hstack_arr.ndim == 4
+        contrast_limits = np.quantile(
+            hstack_arr[hstack_arr.shape[0] // 2, hstack_arr.shape[1] // 2, :, :],
+            [0.5, 0.9999],
+        )
+        layer = self._viewer.add_image(
+            hstack_arr,
+            contrast_limits=contrast_limits,
+            multiscale=False,
+            colormap=cmap,
+            **kwargs,
+        )
+        self._set_dimensions()
         return layer
 
     def load_nrrd(self, path=None, cmap="magenta", **kwargs):
@@ -133,3 +209,82 @@ class NapariBrainViewer:
             raise NotImplementedError()
 
         self._h5widget = self._viewer.window.add_dock_widget(HDF5_Browser(self))
+
+    def select_rect_ROI(self, width=150, height=150):
+        from qtpy.QtWidgets import QPushButton
+
+        # preparing default ROI
+        # size = self._viewer.dims.nsteps
+        # xmid, ymid = size[2] // 2, size[3] // 2
+        # xmin, xmax = xmid - width // 2, xmid + width // 2
+        # ymin, ymax = ymid - height // 2, ymid + height // 2
+        xmin, xmax = 0, width
+        ymin, ymax = 0, height
+        rectangle = np.array(
+            [
+                [xmin, ymin],
+                [xmin, ymax],
+                [xmax, ymax],
+                [xmax, ymin],
+            ]
+        )
+
+        # creating ROI layer
+        roi_layer = self._viewer.add_shapes(
+            rectangle,
+            name="ROI",
+            edge_color="#fff",
+            edge_width=5,
+            face_color=[0, 0, 0, 0],
+            # scale=px,
+        )
+        roi_layer.mode = "select"
+
+        # creating UI
+        qtbutton = QPushButton("Select Area")
+        button = self._viewer.window.add_dock_widget(qtbutton, area="left")
+
+        def clicked():
+            if roi_layer.nshapes != 1:
+                show_warning("Only 1 rectangle allowed !")
+            if roi_layer.shape_type[0] != "rectangle":
+                show_warning("Only rectangle shape allowed !")
+            button.close()
+            show_info("gathering ROI to NapariBrainViewer._ROI")
+            roi_layer.mode = "pan_zoom"
+            xyROI = np.sort(roi_layer.corner_pixels, axis=0)
+            # z = self._viewer.dims.current_step[0]
+            # self._ROI = tuple(xyROI.flatten()) + (z,)
+            self._ROI = {
+                "type": "rectangle",
+                "x": xyROI[:, 0],
+                "y": xyROI[:, 1],
+                "z": self._viewer.dims.current_step[0],
+                "t": self._viewer.dims.current_step[1],
+            }
+
+        qtbutton.clicked.connect(clicked)
+
+    def apply_region_to_hyperstack(self, layer=None, roi=None):
+        if roi is None:
+            if hasattr(self, "_ROI"):
+                roi = self._ROI
+            else:
+                raise TypeError()
+
+        if layer is None:
+            layer = self._viewer.layers.selection.active
+            hyperstack = layer.data
+
+        if roi["type"] == "rectangle":
+            # xmin, ymin, xmax, ymax, z = roi
+            region = hyperstack[
+                roi["z"],
+                :,
+                roi["x"][0] : roi["x"][1],
+                roi["y"][0] : roi["y"][1],
+            ]
+            return region
+
+        else:
+            raise NotImplementedError()
